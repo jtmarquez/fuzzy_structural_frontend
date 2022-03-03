1c821b3cea120698063701ceeb23ec61d9438eea
José Tomás Márquez <jtmarquez@uc.cl>
2022-03-03 13:12:43 -0300

    add Link to home button

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/button_hover.css'
 
 export default function Home() {
@@ -8,9 +9,11 @@ export default function Home() {
         <div>
           <p className='title has-text-white'>Simple fuzzy logic classificator app over structure state </p>
           <p className='subtitle my-5 has-text-white-ter'>Developed by Ignacio Márquez and José Tomás Márquez</p>
-          <div className='my-5 ml-5'>
-            <button id = 'home-button-hover' className='button ml-5 is-white is-large is-rounded is-black'>Take me to the app!</button>
-          </div>
+          <Link to='/fuzzify'>
+            <div className='my-5 ml-5'>
+              <button id = 'home-button-hover' className='button ml-5 is-white is-large is-rounded is-black'>Take me to the app!</button>
+            </div>
+          </Link>
         </div>
       </div>
     </section>
